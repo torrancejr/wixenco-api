7268ae5b565065d41526a4ea3543b99c972c0ef6
Ryan <torrance.jr@gmail.com>
2024-06-05 13:45:53 -0400

    cors fix take 2

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,17 @@ module WixencoApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Middleware for CORS handling
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://www.wixenco.com'
+        resource '*',
+                 headers: :any,
+                 methods: [:get, :post, :put, :patch, :delete, :options, :head],
+                 expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+                 max_age: 600
+      end
+    end
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,17 @@
 
 # config/initializers/cors.rb
 
+# config/initializers/cors.rb
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://www.wixenco.com'  # Your frontend URL
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
-             credentials: true
+             expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+             max_age: 600
   end
 end
+
 
