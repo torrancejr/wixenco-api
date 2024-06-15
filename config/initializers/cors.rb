8b9f97c6fbd956074fa2c76eb843dea0492617e5
Ryan <torrance.jr@gmail.com>
2024-06-15 11:55:06 -0400

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,8 +11,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'https://www.wixenco.com'
-    origins 'localhost:3001'#
+    origins 'https://www.wixenco.com'
+    # origins 'localhost:3001'#
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
